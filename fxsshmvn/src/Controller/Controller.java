1ab0a5390d6d919b89d2c527f05fb7ab5606b6eb
javawork <javawork@javaworkPC>
2016-03-16 22:43:13 +0200

    first

---
+++ fxsshmvn/src/Controller/Controller.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author javawork
+ */
+public class Controller {
+    @FXML
+    Button Save;
+    @FXML
+    Button Connect;
+    @FXML
+    Button Execute;
+    @FXML
+    TextField Host;
+    @FXML
+    TextField Username;
+    @FXML
+    TextField Command;
+    @FXML
+    PasswordField Password;
+    @FXML
+    Label Consoleout;
+    @FXML
+    ListView ServerList;
+    public void SavePressed(ActionEvent e){
+        
+    }
+    public void ConnectPressed(ActionEvent e){
+    
+    }
+    public void ExecutePressed(ActionEvent e){
+    
+    }
+}
